7f70f72681bd578a200e98f4291f25dcc01448d0
minjin.kim <gim162913@gmail.com>
2023-11-14 21:26:52 +0900

    [Minjin.Kim] [CON-33] Base 바텀 시트 화면 작성 (3) - 유저 정보 최신화 작업

--- data/src/main/java/com/connectcrew/data/di/ApiModule.kt
+++ data/src/main/java/com/connectcrew/data/di/ApiModule.kt
@@ -3,11 +3,13 @@ package com.connectcrew.data.di
 import com.connectcrew.data.service.AuthApi
 import com.connectcrew.data.service.ExternalApi
 import com.connectcrew.data.service.ProjectApi
+import com.connectcrew.data.service.UserApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.create
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -19,6 +21,12 @@ internal object ApiModule {
     fun provideAuthApi(
         @TeamOneApi retrofit: Retrofit
     ): AuthApi = retrofit.create(AuthApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideUserApi(
+        @TeamOneApi retrofit: Retrofit
+    ): UserApi = retrofit.create(UserApi::class.java)
 
     @Provides
     @Singleton
