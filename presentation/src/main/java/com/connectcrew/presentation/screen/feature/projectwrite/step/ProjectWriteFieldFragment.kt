e61fb161c48700833e8390d9b9a5f5c4eac5ace2
minjin.kim <gim162913@gmail.com>
2023-12-07 15:28:59 +0900

    [Minjin.Kim] [CON-63] 프로젝트 공고 생성 화면 작성 - 프로젝트 분야 선택 화면 작성

--- presentation/src/main/java/com/connectcrew/presentation/screen/feature/projectwrite/step/ProjectWriteFieldFragment.kt
+++ presentation/src/main/java/com/connectcrew/presentation/screen/feature/projectwrite/step/ProjectWriteFieldFragment.kt
@@ -2,16 +2,85 @@ package com.connectcrew.presentation.screen.feature.projectwrite.step
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.connectcrew.presentation.R
+import com.connectcrew.presentation.adapter.projectwrite.ProjectFieldAdapter
 import com.connectcrew.presentation.databinding.FragmentProjectWriteFieldBinding
 import com.connectcrew.presentation.screen.base.BaseFragment
+import com.connectcrew.presentation.screen.feature.projectwrite.ProjectWriteContainerViewModel
+import com.connectcrew.presentation.util.launchAndRepeatWithViewLifecycle
+import com.connectcrew.presentation.util.listener.setOnSingleClickListener
+import com.connectcrew.presentation.util.safeNavigate
+import com.connectcrew.presentation.util.widget.RecyclerviewItemDecoration
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class ProjectWriteFieldFragment: BaseFragment<FragmentProjectWriteFieldBinding>(R.layout.fragment_project_write_field) {
+class ProjectWriteFieldFragment : BaseFragment<FragmentProjectWriteFieldBinding>(R.layout.fragment_project_write_field) {
+
+    private val projectWriteContainerViewModel: ProjectWriteContainerViewModel by viewModels({ requireParentFragment().requireParentFragment() })
+
+    private val projectWriteFiledAdapter by lazy {
+        ProjectFieldAdapter(projectWriteContainerViewModel::setProjectFiled)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        dataBinding.apply {
+            viewModel = projectWriteContainerViewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+        initView()
+        initListener()
+        initObserver()
+    }
+
+    private fun initView() {
+        dataBinding.rvProjectField.apply {
+            addItemDecoration(RecyclerviewItemDecoration(0, 20, 0, 20, R.layout.item_project_write_field))
+            adapter = projectWriteFiledAdapter
+            layoutManager = GridLayoutManager(requireContext(), if (resources.getBoolean(R.bool.isTablet)) 5 else 3)
+            itemAnimator = null
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun initListener() {
+        with(dataBinding) {
+            btnPrevious.setOnSingleClickListener {
+                findNavController().navigateUp()
+            }
+
+            btnNext.setOnSingleClickListener {
+                findNavController().safeNavigate(ProjectWriteFieldFragmentDirections.actionProjectWriteFieldFragmentToProjectWritePostFragment())
+            }
+        }
+    }
+
+    private fun initObserver() {
+        launchAndRepeatWithViewLifecycle {
+            launch {
+                combine(
+                    projectWriteContainerViewModel.projectContainerInfo.filterNotNull(),
+                    projectWriteContainerViewModel.projectSelectedFiled,
+                    ::Pair
+                ).collect { (projectInfoList, selectInfo) ->
+                    projectWriteFiledAdapter.submitList(projectInfoList.category.map { filed -> filed.copy(isSelected = selectInfo.any { it.category == filed.category }) })
+                }
+            }
+
+            launch {
+                projectWriteContainerViewModel.messageRes.collect {
+                    showToast(it)
+                }
+            }
+        }
     }
 }
 
