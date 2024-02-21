13a2e4e3ed9398ac50c754c71bce856d577bab93
DoHyeok Kim <hyeokdokim@gmail.com>
2024-02-21 15:14:45 +0900

    [Dohyeok.Kim] [CON-67] 프로젝트 팀원 화면 작성

---
+++ presentation/src/main/java/com/connectcrew/presentation/adapter/project/member/ProjectMemberViewHolder.kt
@@ -0,0 +1,8 @@
+package com.connectcrew.presentation.adapter.project.member
+
+import com.connectcrew.presentation.adapter.DataBindingViewHolder
+import com.connectcrew.presentation.databinding.ItemProjectMemberBinding
+
+class ProjectMemberViewHolder(
+    val binding: ItemProjectMemberBinding
+) : DataBindingViewHolder<ItemProjectMemberBinding>(binding)
